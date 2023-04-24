6a88524a0011b94b2808484a3d69d1cffd91089b
Wa <122730917+STKR2@users.noreply.github.com>
2023-04-25 02:53:30 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -17,7 +17,7 @@ API_ID = int(getenv("API_ID", "8934899"))
 API_HASH = getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
 BOT_TOKEN = getenv("BOT_TOKEN", "5204315005:AAGKqH1rjGG2H9LDqu_n2VzxJhRngaUbFNQ")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "600"))
-STRING_SESSION = getenv("STRING_SESSION", "AgA1Of9vbSmwJVeDwuuhAxlEFAwHPC6feYDKowBCEzd0R5keg1z2ENLSHq0M2xOg-STOVH5ibK_8pij21XCgeqkjf_azOKScbcd8HMUL512oRh45nt_8qix9tdiTjY7maTe0zjohH4oXN7vniAvMnCazd_YRMr2nlVCgfnhqKwFB2mDuHcTdojW6zWezAFA_afOQvuQ8Y79U6n1ejkmCdYNlPKmPhsRl00M8YiqQXQgdpEZJczSql7ZDQlHlxNnVghDe3lNwf4e91apZ_Yrug02rzGM_VqCaltLTVMvcZ6LTuf_VJOhpyStoHKi4hM8zc0XbI9_YAonxKAghi3lP_vXXAAAAAUM3evYA")
+STRING_SESSION = getenv("STRING_SESSION", "AgBp-a0RLvxLtoGMo-lobfwZyjqGKYzTqPWU3_Wn29Rg-dH8wNftfw3VXfrVeM7tdBriw6-xpJLqLOynF-7TyY8gkls2Vi6ILsKtTq2bKkg_iqrNfw51l_kDp6LmK4-cjRF2mm-wEzN-o05_paVXhd1Z7UIAUZZFodEnjZQ8hHrXS77x-NSQE06TuoBRrQXao5xZG2bw8lEbDjetLWIptr7jUDJaxoikSBQjpN0ypXBloy_71kTlNvy2TwIN8-6bqsJti6q4iKgGWm0TRtith_BuF_IS5uBiBhidt89IxOofnqC4tnZpbEtxX6kfqKKnzarHJQvDu_1dn1pvQTR-vn61AAAAAUM3evYA")
 BOT_USERNAME = getenv("BOT_USERNAME", "Saiadjabot")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
@@ -29,7 +29,7 @@ SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
 #•••••••••••••••••••••••• Mongodb Url Stuff & Loggroupid •••••••••••
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001847569598")) 
 
-MONGODB_URL = getenv("MONGODB_URL", " mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
+MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 #________________________ Updates 🍃 & Music bot name________________
 NETWORK = getenv("NETWORK", "xl444")
 GROUP = getenv("GROUP", "xl444")
