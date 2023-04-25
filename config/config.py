2789842326f02b765d9d3496607e6360367d8700
Wa <122730917+STKR2@users.noreply.github.com>
2023-04-25 03:09:12 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,11 +11,11 @@ aiohttpsession = aiohttp.ClientSession()
 que = {}
 admins = {}
 
-#------------------------ Important Stuff 😌 -----------------------
+#------------------------ Important Stuff  -----------------------
 
 API_ID = int(getenv("API_ID", "8934899"))
 API_HASH = getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
-BOT_TOKEN = getenv("BOT_TOKEN", "5204315005:AAGKqH1rjGG2H9LDqu_n2VzxJhRngaUbFNQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "5204315005:AAGPX3ySLQZkvVcLHc7n1N_u0NFJUOHN0lw")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "600"))
 STRING_SESSION = getenv("STRING_SESSION", "AgBp-a0RLvxLtoGMo-lobfwZyjqGKYzTqPWU3_Wn29Rg-dH8wNftfw3VXfrVeM7tdBriw6-xpJLqLOynF-7TyY8gkls2Vi6ILsKtTq2bKkg_iqrNfw51l_kDp6LmK4-cjRF2mm-wEzN-o05_paVXhd1Z7UIAUZZFodEnjZQ8hHrXS77x-NSQE06TuoBRrQXao5xZG2bw8lEbDjetLWIptr7jUDJaxoikSBQjpN0ypXBloy_71kTlNvy2TwIN8-6bqsJti6q4iKgGWm0TRtith_BuF_IS5uBiBhidt89IxOofnqC4tnZpbEtxX6kfqKKnzarHJQvDu_1dn1pvQTR-vn61AAAAAUM3evYA")
 BOT_USERNAME = getenv("BOT_USERNAME", "Saiadjabot")
